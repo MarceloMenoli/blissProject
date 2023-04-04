428532e8d421525f2c1bb0c31a905be6d999a739
MarceloMenoli <marcelomenoli7@gmail.com>
2023-04-04 22:30:40 +0100

    feat: add main routes and first pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Router } from "./routes";
 import GlobalStyle from "./styles/globalStyles";
 
 function App() {
   return (
     <div>
       <GlobalStyle />
-      <div className="App">Hello World!</div>
+      <Router />
     </div>
   );
 }
